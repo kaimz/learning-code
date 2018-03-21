eee58bd4d6c97757d41ac0e5d3abf200e0da4910
”kaim <kaim@qnight.cn>
2018-03-21 10:12:39 +0800

    RecursiveAction

--- fork-join/src/main/java/PrintTask.java
+++ fork-join/src/main/java/PrintTask.java
@@ -1,16 +1,14 @@
+import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveAction;
 
 /**
- * https://www.liaoxuefeng.com/article/001493522711597674607c7f4f346628a76145477e2ff82000
- * http://blog.csdn.net/ouyang_peng/article/details/46491217
- *
  * @author Zhang Kai
  * @version 1.0
  * @since <pre>2018/3/20 18:38</pre>
  */
 // 没有返回值的 fork / join 任务框架
 public class PrintTask extends RecursiveAction {
-    private static final int THRESHOLD = 100;
+    private static final int THRESHOLD = 5;
     private int start;
     private int end;
 
@@ -19,14 +17,23 @@ public class PrintTask extends RecursiveAction {
         this.end = end;
     }
 
+    public static void main(String[] args) {
+        PrintTask task = new PrintTask(0, 25);
+        // 分配四个线程给它
+        ForkJoinPool pool = new ForkJoinPool(4);
+        pool.execute(task);
+        pool.shutdown();
+    }
+
     @Override
     protected void compute() {
-        if (THRESHOLD > (end - start)) {
-            // 满足小任务条件，只分配 100 个打印任务
+        if (THRESHOLD >= (end - start)) {
+            // 满足小任务条件，分配打印任务
             for (int i = start; i < end; i++) {
                 System.out.println(Thread.currentThread().getName() + ": " + i);
             }
         } else {
+            // 任务还能继续拆分
             int division = (start + end) >> 1;
             PrintTask task1 = new PrintTask(start, division);
             PrintTask task2 = new PrintTask(division, end);
