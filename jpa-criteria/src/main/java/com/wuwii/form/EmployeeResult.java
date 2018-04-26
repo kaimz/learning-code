cc0583ba94a70da9bbc10341be4baab417877640
kaimz <k@wuwii.com>
2018-04-26 15:02:22 +0800

    修改 BUG

--- jpa-criteria/src/main/java/com/wuwii/form/EmployeeResult.java
+++ jpa-criteria/src/main/java/com/wuwii/form/EmployeeResult.java
@@ -3,13 +3,17 @@ package com.wuwii.form;
 import com.wuwii.entity.Employee;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 /**
  * @author Zhang Kai
  * @version 1.0
  * @since <pre>2018/3/8 19:24</pre>
  */
+@EqualsAndHashCode(callSuper = true)
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class EmployeeResult extends Employee {
     private String name;
--- jpa-criteria/src/test/java/com/wuwii/controller/EmployeeController1Test.java
+++ jpa-criteria/src/test/java/com/wuwii/controller/EmployeeController1Test.java
@@ -1,5 +1,6 @@
 package com.wuwii.controller;
 
+import com.wuwii.form.EmployeeResult;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
@@ -7,6 +8,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -24,8 +27,13 @@ public class EmployeeController1Test {
 
     @Test
     public void listAll() {
-        ResponseEntity<List> result = restTemplate.getForEntity("/emp", List.class);
-        Assert.assertThat(result.getBody(), Matchers.notNullValue());
+        // 由于我返回的是 List 类型的，一直想不到办法解决，网上给出了解决办法，使用 exchange 函数代替
+        //public <T> ResponseEntity<T> exchange(String url, HttpMethod method,
+        //			HttpEntity<?> requestEntity, ParameterizedTypeReference<T> responseType,
+        //			Object... urlVariables) throws RestClientException {
+        ParameterizedTypeReference<List<EmployeeResult>> type = new ParameterizedTypeReference<List<EmployeeResult>>() {};
+        ResponseEntity<List<EmployeeResult>> result = restTemplate.exchange("/emp", HttpMethod.GET, null, type);
+        Assert.assertThat(result.getBody().get(0).getName(), Matchers.notNullValue());
     }
 
 }
